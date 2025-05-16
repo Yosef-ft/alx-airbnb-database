1a7d12a2071d5a88cad6a824aaca4e03151e4f50
Yosef <yosefetene02@gmail.com>
2025-05-16 23:38:34 +0300

    Added queries with joins

---
+++ database-adv-script/joins_queries.sql
@@ -0,0 +1,23 @@
+
+-- Inner join: fetch all bookings and respective users 
+
+SELECT users.first_name, users.last_name,
+bookings.start_date, bookings.end_date, bookings.status, bookings.total_price
+from users INNER JOIN bookings
+on users.user_id = bookings.user_id;
+
+
+
+-- Left Join: retrieve all properties and their reviews
+
+SELECT properties.name, properties.description, properties.location,
+properties.location, properties.price_per_night from properties
+LEFT JOIN reviews on reviews.property_id = properties.property_id;
+
+
+-- Full outer join: retrieve all users and all bookings
+
+SELECT users.first_name, users.last_name,
+bookings.start_date, bookings.end_date, bookings.status, bookings.total_price
+from users FULL OUTER JOIN bookings
+on users.user_id = bookings.user_id;
