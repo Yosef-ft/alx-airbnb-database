4528873748fd3314ccacb8afc42fc532c375641c
Yosef <yosefetene02@gmail.com>
2025-05-17 13:24:42 +0300

    Minor changes

--- database-adv-script/subqueries.sql
+++ database-adv-script/subqueries.sql
@@ -1,23 +1,23 @@
 
--- All properties where the average rating is greater than 4.0
+-- All properties WHERE the average rating is greater than 4.0
 
 
 SELECT * from properties 
-where properties.property_id IN(
+WHERE properties.property_id IN(
   SELECT property_id
   from reviews 
   GROUP BY property_id
-  HAVING avg(rating) > 4
+  HAVING AVG(rating) > 4
 );
 
 -- Users who have made more than 3 bookings
 
 SELECT * FROM users
-where user_id IN (
+WHERE user_id IN (
   SELECT  user_id
   from bookings
   group by user_id
-  having count(user_id) > 3
+  having COUNT(user_id) > 3
 );
 
 
