761563514fec1419be2fd7062a9ec2a14ca72e66
Yosef <yosefetene02@gmail.com>
2025-05-17 08:43:44 +0300

    Added Aggregations and Window Functions

---
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -0,0 +1,16 @@
+
+-- Total number of bookings made by each user
+
+SELECT count(booking_id), user_id FROM bookings
+GROUP BY user_id;
+
+
+-- Rank properties based on the total number of bookings they have received
+
+SELECT *, RANK() OVER (ORDER BY total_bookings DESC) as rank_no
+From(
+    SELECT  properties.name, COUNT(bookings.property_id) AS total_bookings
+    FROM bookings
+    INNER JOIN properties on properties.property_id = bookings.property_id
+    GROUP BY  properties.name
+) AS sub;
